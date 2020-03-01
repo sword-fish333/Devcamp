c0e1481f3487e21d4da538d5929d6f52e2335712
sword-fish333 <ghiurcaalin@gmail.com>
2020-03-01 12:15:35 +0200

    b

--- server.js
+++ server.js
@@ -5,6 +5,26 @@ const dotenv=require('dotenv');
 dotenv.config({path:'./config/config.env'});
 
 const app=express();
+app.get('/api/v1/courses',(req,res)=>{
+res.status(200).json({status:true,msg:'Show all courses'})
+});
+app.get('/api/v1/courses/:id',(req,res)=>{
+    res.status(200).json({success:true,msg:`Get course ${req.params.id}`})
+});
+
+app.post('/api/v1/courses',(req,res)=>{
+   res.status(200).json({success:true,msg:"Course created"})
+});
+
+
+app.put('/api/v1/courses/:id',(req,res)=>{
+    res.status(200).json({success:true,msg:`Course updated ${req.params.id}`})
+});
+
+app.delete('/api/v1/courses/:id',(req,res)=>{
+    res.status(200).json({success:true,msg:`Course deleted ${req.params.id}`})
+
+});
 const PORT=process.env.PORT || 5000;
 
-app.listen(PORT,console.log(`Server is running in ${process.env.NODE_ENV} mode  on port ${PORT}`));
+app.listen(PORT,console.log(`Server is running in ${process.env.NODE_ENV} mode   on port ${PORT}`));
